a47708a2391473fa9838455771f5932568b503bf
Riperto Adriel Aaron <aaron.ariperto@gmail.com>
2024-05-16 00:34:39 -0300

    add oil and remove nonels

--- .config/nvim/lua/himuraxkenji/lazy.lua
+++ .config/nvim/lua/himuraxkenji/lazy.lua
@@ -1,31 +1,31 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	{ import = "himuraxkenji.plugins" },
-	{ import = "himuraxkenji.plugins.lsp" },
-	{ import = "himuraxkenji.plugins.coding" },
-	{ import = "himuraxkenji.plugins.ui" },
-	{ import = "himuraxkenji.plugins.git" },
+  { import = "himuraxkenji.plugins" },
+  -- { import = "himuraxkenji.plugins.lsp" },
+  { import = "himuraxkenji.plugins.coding" },
+  { import = "himuraxkenji.plugins.ui" },
+  { import = "himuraxkenji.plugins.git" },
 }, {
-	install = {
-		colorscheme = { "nightfly" },
-	},
-	checker = {
-		enabled = true,
-		notify = false,
-	},
-	change_detection = {
-		notify = false,
-	},
+  install = {
+    colorscheme = { "nightfly" },
+  },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
 })
--- .config/nvim/lua/himuraxkenji/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/himuraxkenji/plugins/lsp/lspconfig.lua
@@ -15,27 +15,6 @@ return {
     build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
   {
-    "nvimtools/none-ls.nvim",
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.diagnostics.erb_lint,
-          null_ls.builtins.diagnostics.eslint_d,
-          null_ls.builtins.diagnostics.rubocop,
-          null_ls.builtins.formatting.rubocop,
-          null_ls.builtins.formatting.gofumpt,
-          null_ls.builtins.formatting.goimports,
-          -- null_ls.builtins.formatting.golines,
-        },
-      })
-
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
